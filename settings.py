18f108c018b41968e7e1439b61df5df50eea4101
HarryW <wangaoharry@hotmail.com>
2023-03-28 18:56:49 -0700

    🔧 Add settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+# sqlite3 database (relative to the project directory)
+DATABASES = "sqlite:///db.sqlite3"
+
+# EXCHANGE
+EXCHANGE = "binance"
+PAIRLIST = [
+    "BTC/USDT",
+    "ETH/USDT",
+    "BNB/USDT",
+]
+
